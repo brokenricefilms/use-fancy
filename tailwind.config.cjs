5660227c319c34330ba9f82dda7f0093a160c6c0
Thuan Pham <thuanowa@gmail.com>
2023-05-10 20:33:43 +0700

    refactor: jetbrain fontsource bye google font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,11 +3,6 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   darkMode: 'class',
   theme: {
-    extend: {
-      fontFamily: {
-        mplus: ["'M PLUS Rounded 1c'", 'Verdana', 'sans-serif']
-      }
-    }
   },
   plugins: []
 }
